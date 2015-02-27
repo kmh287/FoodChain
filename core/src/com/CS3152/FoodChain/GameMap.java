e3232f8d7d41880216eecd6c1f50a4199df25faa
Christian Choi <cyc47@users.noreply.github.com>
2015-02-26 19:37:45 -0500

    Removed Debug Printing
    Commented out the print statement in GameMap for debugging and removed
    buggy text.

--- core/src/com/CS3152/FoodChain/GameMap.java
+++ core/src/com/CS3152/FoodChain/GameMap.java
@@ -170,10 +170,7 @@ public class GameMap {
     public int mapYToScreen(int yTileIndex){
         int screenHeight = Gdx.graphics.getHeight();
         int yIncrement = (screenHeight - UI_OFFSET) / layout.length;
-<<<<<<< HEAD
-        System.out.println(yTileIndex * yIncrement + UI_OFFSET);
-=======
->>>>>>> master
+        //System.out.println(yTileIndex * yIncrement + UI_OFFSET);
         return yTileIndex * yIncrement + UI_OFFSET;
     }
     
