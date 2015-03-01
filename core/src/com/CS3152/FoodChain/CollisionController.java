b508896f674310813aa50948b3fcd8d8da72fe98
Christian Choi <cyc47@users.noreply.github.com>
2015-03-01 00:02:25 -0500

    Merge CollisionController, InputController, PlayerController

---
+++ core/src/com/CS3152/FoodChain/CollisionController.java
@@ -0,0 +1,17 @@
+package com.CS3152.FoodChain;
+
+import java.util.Random;
+import com.badlogic.gdx.math.*;
+
+/**
+ * Class to handle basic collisions in the game.
+ *
+ * This is the simplest of physics engines.  In later labs, we will see how to work 
+ * with more interesting engines.
+ *
+ * As a major subcontroller, this class must have a reference to all the models.
+ */
+
+public class CollisionController {
+
+}
