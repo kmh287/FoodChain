4cc693710a6f1c9279258e6a710e8f76e55a0488
ashtonhsu20 <amh333@cornell.edu>
2015-03-01 23:04:35 -0500

    collisions animal-player

--- core/src/com/CS3152/FoodChain/CollisionController.java
+++ core/src/com/CS3152/FoodChain/CollisionController.java
@@ -3,6 +3,7 @@ package com.CS3152.FoodChain;
 import java.util.List;
 import java.util.Random;
 
+import java.util.Random;
 import com.badlogic.gdx.math.*;
 
 /**
@@ -25,6 +26,15 @@ public class CollisionController {
 	
 	private Vector2 tmp;
 	
+	/** Caching object for computing normal */
+	private Vector2 normal;
+
+	/** Caching object for computing net velocity */
+	private Vector2 velocity;
+	
+	private float distance;
+	
+	private boolean canMove;
 	/**
 	 * Creates a CollisionController for the given models.
 	 * 
@@ -36,11 +46,10 @@ public class CollisionController {
 		animals = a;
 		
 		tmp = new Vector2();
+		normal= new Vector2();
+		velocity = new Vector2();
 	}
 	
-	/**
-	 * Update the Hunter and all Animals, moving them forward.
-	 */
 	public void update() {
 		moveIfPossible(hunter);
 		for(Animal a : animals) {
@@ -50,8 +59,21 @@ public class CollisionController {
 	
 	private void moveIfPossible(Hunter hunter) {
 		tmp.set(hunter.getxPos(), hunter.getyPos());
-		tmp.add(hunter.getVX(), hunter.getVY());
-		hunter.setPosition(tmp);
+		tmp.add(hunter.getVX(), hunter.getVY());		
+		canMove=true;
+		for(Animal a : animals){
+			normal.set(hunter.getxPos()-a.getxPos(), hunter.getyPos()-a.getyPos());
+			distance = normal.len();
+			normal.nor();
+			if (distance<30){
+				canMove=false;
+				tmp.set(normal).scl((1 - distance) / 2);  // normal * (d1 - dist)/2
+				hunter.setPosition(hunter.getPosition().sub(tmp));
+			}
+		}
+		if(canMove){
+			hunter.setPosition(tmp);
+		}
 	}
 	
 	private void moveIfPossible(Animal animal) {
