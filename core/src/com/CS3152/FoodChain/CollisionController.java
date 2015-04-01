62d3deeeefc8f6cb15d3157f16ababb608c51b5c
Christian Choi <cyc47@users.noreply.github.com>
2015-04-01 17:40:31 -0400

    Constant updates

--- core/src/com/CS3152/FoodChain/CollisionController.java
+++ core/src/com/CS3152/FoodChain/CollisionController.java
@@ -116,7 +116,7 @@ public class CollisionController implements ContactListener {
 
 
 	public void update() {
-		world.step(1/60f, 3, 3);
+		//world.step(1/60f, 3, 3);
 		
 		//Updates the animals' actions
 		//i is the index of each animal AI in controls
@@ -134,7 +134,7 @@ public class CollisionController implements ContactListener {
 			}
 			//System.out.println(o.getPosition().toString());
 		}
-		//world.step(1/60f, 6, 2);
+		world.step(1/60f, 3, 3);
 		//checkTrapped();
 	}
 	
