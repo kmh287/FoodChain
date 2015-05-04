4e87ad915e51f5017cd2fdc53a1deb0abf1e960b
Christian Choi <cyc47@users.noreply.github.com>
2015-05-04 06:02:46 -0400

    New level HideAndSeek and changes to GDXRoot

--- core/src/com/CS3152/FoodChain/GDXRoot.java
+++ core/src/com/CS3152/FoodChain/GDXRoot.java
@@ -75,17 +75,26 @@ public class GDXRoot extends Game implements ScreenListener {
 		levelListTutorial.add("tutorial1");
 		levelListTutorial.add("tutorial2");
 		levelListTutorial.add("tutorial3");
+		levelListTutorial.add("HideAndSeek");
 		levelListTutorial.add("Cycle2");
 		//1 levels
 		levelList1.add("tutorial1");
 		levelList1.add("tutorial2");
+		levelList1.add("tutorial3");
+		levelList1.add("Cycle2");
+		levelList1.add("HideAndSeek");
 		//levelList1.add("kpatroltest2");
 		//2 levels
+		levelList2.add("HideAndSeek");
 		levelList2.add("tutorial1");
 		levelList2.add("tutorial2");
+		levelList2.add("tutorial3");
+		levelList2.add("Cycle2");
 		//3 levels
 		levelList3.add("tutorial1");
 		levelList3.add("tutorial2");
+		levelList3.add("tutorial3");
+		levelList3.add("Cycle2");
 		
 		List<List<String>> allLists = new ArrayList<List<String>>();
 		allLists.add(levelListTutorial);
